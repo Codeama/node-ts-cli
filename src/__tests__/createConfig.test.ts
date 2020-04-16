dd883f95f4bb2468149d6e55aa1c4561742a2aeb
Bukola Jimoh <bukolajimoh@economist.com>
2020-04-16 07:00:20 +0100

    Add test cases

---
+++ src/__tests__/createConfig.test.ts
@@ -0,0 +1,6 @@
+describe('Create config files', () => {
+  it.todo('should create a .prettierrc file');
+  it.todo('should create a jestconfig.json file');
+  it.todo('should create a tsconfig.json file');
+  it.todo('should create a tslint.json file');
+});
---
+++ src/__tests__/npm-git-init.test.ts
@@ -0,0 +1,8 @@
+describe('NPM init', () => {
+  it.todo('should have a package.json file');
+});
+
+describe('Git init', () => {
+  it.todo('should have a .gitignore file');
+  it.todo('should have a README.md file');
+});
