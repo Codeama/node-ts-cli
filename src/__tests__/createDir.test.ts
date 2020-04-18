348eaa3c560c3bacc9bd14d409df1fba3ed24979
Bukola Jimoh <bukolajimoh@economist.com>
2020-04-18 08:03:44 +0100

    start writing tests and implement createDir function

--- src/__tests__/createDir.test.ts
+++ src/__tests__/createDir.test.ts
@@ -1,5 +1,27 @@
+import { createJsDir } from '../createDir';
+
 describe('Create Directory', () => {
-  it.todo('should create a project directory when one is specified');
+  it('should create a directory with specified name', () => {
+    let entry = 'test';
+    // fake node fs function ---mkdirSync(arg)
+    function fakemkdirFunc(name: string) {
+      entry = `${name}-directory`;
+    }
+    function getmkdirSync(name: string) {
+      return fakemkdirFunc(name);
+    }
+    
+    createJsDir(getmkdirSync, entry);
+    expect(entry).toEqual('test-directory');
+  });
+
+  it('should return an error when something goes wrong', () => {
+    function getmkdirSync(name: string) {
+      throw new Error();
+    }
+    const error = createJsDir(getmkdirSync, 'test');
+    expect(error).toEqual(new Error());
+  })
   it.todo('should use current working directory when none is specified');
   it.todo('should not create another directory when the specified name already exists');
 });
---
+++ src/createDir.ts
@@ -0,0 +1,13 @@
+import { mkdirSync } from 'fs';
+
+export function getmkdirSync(name: string) {
+  return mkdirSync(name);
+}
+
+export function createJsDir(fn: (conf: string) => void, name: string) {
+  try {
+    fn(name); 
+  } catch (err) {
+    return err;
+  }
+}
