8dc5a1c212451c84e12e0afc6f975eb20a7e01db
Bukola Jimoh <bukolajimoh@economist.com>
2020-04-20 06:45:42 +0100

    Add integration test cases

---
+++ src/integration-tests/index.ts
@@ -0,0 +1,23 @@
+describe('Files and Directories', () => {
+  describe('NPM', () => {
+    it.todo('should have a package.json file');
+  });
+
+  describe('Git', () => {
+    it.todo('should have a .gitignore file');
+    it.todo('should have a README.md file');
+  });
+
+  describe('Prettier', () => {
+    it.todo('should have a .prettierrc file');
+  });
+
+  describe('TypesScript files', () => {
+    it.todo('should create a tsconfig.json file');
+    it.todo('should create a tslint.json file');
+  });
+
+  describe('Jest', () => {
+    it.todo('should have a jestconfig.json file');
+  });
+});
