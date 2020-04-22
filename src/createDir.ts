9bd159eed5f7610fc9e60580e5d0f6604bccf874
Bukola Jimoh <bukolajimoh@economist.com>
2020-04-22 06:57:57 +0100

    Update error handling

--- src/createDir.ts
+++ src/createDir.ts
@@ -1,13 +1,21 @@
 import { mkdirSync } from 'fs';
+import { yellow, red } from 'chalk';
+const { log } = console;
 
 export function getmkdirSync(name: string) {
-  return mkdirSync(name);
+  try {
+    return mkdirSync(name);
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      log(yellow(`A directory named ${name} already exists.`));
+    } else {
+      process.on('exit', (code) => {
+        log(red(`Something went wrong: ${err.message}\nExit code ${code}`));
+      });
+    }
+  }
 }
 
 export function createJsDir(fn: (conf: string) => void, name: string) {
-  try {
-    fn(name);
-  } catch (err) {
-    return err;
-  }
+  fn(name);
 }
