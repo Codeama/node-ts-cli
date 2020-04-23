af2de4f2a1d51da384eb0b358d8bb4d6bc735f0b
Bukola Jimoh <bukolajimoh@economist.com>
2020-04-23 05:23:45 +0100

    Update-II error tests createDir

--- src/__tests__/createDir.test.ts
+++ src/__tests__/createDir.test.ts
@@ -1,4 +1,4 @@
-import { getmkdirSync, createJsDir } from '../createDir';
+import { createJsDir } from '../createDir';
 
 describe('Create Directory', () => {
   it('should create a directory with specified name', () => {
@@ -16,8 +16,10 @@ describe('Create Directory', () => {
   });
 
   it('should return an error if something goes wrong', () => {
-    expect(getmkdirSync).toThrowError();
-    expect(createJsDir).toThrowError();
+    function getmkdirSyncMocked(name: string) {
+      throw new Error();
+    }
+    expect(() => createJsDir(getmkdirSyncMocked, 'test_directory')).toThrowError();
   });
   it.todo('should use current working directory when none is specified');
   it.todo('should not create another directory when the specified name already exists');
