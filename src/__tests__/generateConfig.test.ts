dfbd68301f50f8798e936786c7b422cc863e4240
Bukola Jimoh <bukolajimoh@economist.com>
2020-04-29 14:41:53 +0100

    Update generateConfig test

--- src/__tests__/generateConfig.test.ts
+++ src/__tests__/generateConfig.test.ts
@@ -1,25 +1,17 @@
-import { generateConfigFile } from '../generateConfig';
+import { getCopyAllFilesFunc } from '../generateConfig';
 
-describe('Create config files', () => {
-  it('should generate new file', () => {
-    let copy;
-    // fake copy function
-    function fakecopyFile(src: string, dest: string) {
-      copy = src;
+describe('getCopyAllFilesFunc', () => {
+  it('should return an array of files copied', async () => {
+    // not an actual mimic of the copy function in utilities
+    async function fakeCopyFileFunc(src: string, dest: string) {
+      const copy = src;
+      return Promise.resolve(copy);
     }
-
-    function getCopyFile(src: string, dest: string) {
-      fakecopyFile(src, dest);
-    }
-    const source = 'source_file';
-    generateConfigFile(getCopyFile, source, '');
-    expect(copy).toEqual(source);
-  });
-
-  it('should throw an error if something goes wrong', () => {
-    function fakecopyFile(src: string, dest: string) {
-      throw new Error();
-    }
-    expect(() => generateConfigFile(fakecopyFile, 'src', 'dest')).toThrowError();
+    const files = Array('file1', 'file2', 'file3');
+    const srcDir = 'src-dir';
+    const destDir = 'dest-dir';
+    const copyFile = getCopyAllFilesFunc(srcDir, destDir);
+    const result = await copyFile(fakeCopyFileFunc, files);
+    expect(result).toEqual(['src-dir', 'src-dir', 'src-dir']);
   });
 });
