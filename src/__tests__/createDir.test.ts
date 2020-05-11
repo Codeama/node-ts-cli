c5987999309be0360c21b765809be9f78e069174
Bukola Jimoh <bukolajimoh@economist.com>
2020-05-11 07:22:03 +0100

    refactor and update tests as integration

--- src/__tests__/createDir.test.ts
+++
@@ -1,6 +0,0 @@
-it.todo(
-  'should use current working directory when none is specified',
-);
-it.todo(
-  'should not create another directory when the specified name already exists',
-);
--- src/__tests__/npm-git-init.test.ts
+++
@@ -1,8 +0,0 @@
-describe('NPM init', () => {
-  it.todo('should have a package.json file');
-});
-
-describe('Git init', () => {
-  it.todo('should have a .gitignore file');
-  it.todo('should have a README.md file');
-});
