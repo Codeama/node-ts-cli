ef41cc736282bc838f3c50943d9468ab504d787b
Bukola Jimoh <bukolajimoh@economist.com>
2020-05-15 08:32:36 +0100

    Move commands to separate file

---
+++ src/commands.ts
@@ -0,0 +1,15 @@
+/**
+ * Command to install typescript
+ */
+export const typeScript =
+  'npm install typescript @types/node --save-dev';
+/**
+ * Command to install linters
+ */
+export const lintScript =
+  'npm install prettier ts-lint  tslint-config-prettier --save-dev';
+/**
+ * Command to install jest
+ */
+export const jestScript =
+  'npm install jest @types/jest ts-jest --save-dev';
